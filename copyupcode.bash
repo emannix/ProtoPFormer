d794b171b73f200c1f6b5d06096ba5cb83d54623
Evelyn Mannix <nathaniel.bloomfield@unimelb.edu.au>
2024-10-23 18:36:51 +1100

    fixing up copying stuff

--- copyupcode.bash
+++ copyupcode.bash
@@ -1,2 +1,2 @@
 
-rsync -avh --exclude '.git'  --exclude '__pycache__' --exclude 'checkpoints' --exclude 'runs'  . nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/ProtoPFormer
+rsync -avh --exclude '.git'  --exclude '__pycache__' --exclude 'checkpoints' --exclude 'output_cosine'  . nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/ProtoPFormer
