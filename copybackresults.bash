71f314bbbfc5873f1cbe4edba5c0ce78ba7dd4f2
Evelyn Mannix <nathaniel.bloomfield@unimelb.edu.au>
2024-10-23 14:46:48 +1100

    have running working

---
+++ copybackresults.bash
@@ -0,0 +1,5 @@
+
+rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/ProtoPFormer/runs ./runs
+
+
+
---
+++ copyupcode.bash
@@ -0,0 +1,2 @@
+
+rsync -avh --exclude '.git'  --exclude '__pycache__' --exclude 'checkpoints' --exclude 'runs'  . nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/ProtoPFormer
